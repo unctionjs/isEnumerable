90784acf4552cab66799a62f66e8bef2db99e787
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:47 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,26 @@
+
+import isEnumerable from "./index.ts";
+
+test("Array", () => {
+  expect(isEnumerable([])).toEqual(true);
+});
+
+test("Object", () => {
+  expect(isEnumerable({})).toEqual(true);
+});
+
+test("Set", () => {
+  expect(isEnumerable(new Set())).toEqual(true);
+});
+
+test("Map", () => {
+  expect(isEnumerable(new Map())).toEqual(true);
+});
+
+test("String", () => {
+  expect(isEnumerable("")).toEqual(true);
+});
+
+test("Non-enumerable", () => {
+  expect(isEnumerable(false)).toEqual(false);
+});
